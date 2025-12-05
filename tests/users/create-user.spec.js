a4130310781fc3cac4c12314d7b2305b9ed3b878
Adam <wudarczykadam@gmail.com>
2025-12-05 20:23:43 +0100

    add first API test POST /users

---
+++ tests/users/create-user.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+const BASE_URL = 'https://gorest.co.in/public/v2';
+const TOKEN = process.env.GOREST_TOKEN;
+
+test.describe('GoREST API - Create User', () => {
+
+    test('POST /users → create a new user', async ({ request }) => {
+        const response = await request.post(`${BASE_URL}/users`, {
+            headers: {
+                Authorization: `Bearer ${TOKEN}`
+            },
+            data: {
+                name: "Adam Tester",
+                gender: "male",
+                email: `adam_${Date.now()}@mail.com`,
+                status: "active"
+            }
+        });
+
+        expect(response.status()).toBe(201);
+
+        const body = await response.json();
+        expect(body).toHaveProperty("id");
+        expect(body).toHaveProperty("email");
+        expect(body.name).toBe("Adam Tester");
+    });
+});
